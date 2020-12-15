005d8a4a011cb384ddd71481668041683bed9eea
anselbrandt <ansel@anselbrandt.com>
2020-12-15 16:09:05 -0500

    Serve html and API from same port

--- src/test.ts
+++ src/test.ts
@@ -20,7 +20,7 @@ const main = async () => {
   const ip = await internalIp.v4();
   console.log("server ip:", ip);
   devices.forEach((device) => {
-    subscribe({ address: device.address, ip: ip!, port: `${+PORT + 1}` });
+    subscribe({ address: device.address, ip: ip!, port: `${PORT}` });
   });
 
   const app = express();
@@ -28,30 +28,26 @@ const main = async () => {
   app.get("/", (_, res) => {
     res.sendFile(path.join(__dirname + "../web/build/index.html"));
   });
-  const httpServer = http.createServer(app);
-  httpServer.listen(PORT, () => {
-    console.log(`server started on http://localhost:${PORT}`);
-  });
 
   app.get("/api", (_, res) => {
     res.send("hello from the API server.");
   });
 
-  app
-    .use(parser.xml())
-    .all("/wemo", (request, response) => {
-      const sid = request.headers.sid;
-      if (sid) {
-        const binaryState = request.body["e:propertyset"]["e:property"][0];
-        console.log(sid, binaryState);
-        response.sendStatus(200);
-      } else {
-        response.send("hello from the API server.");
-      }
-    })
-    .listen(`${+PORT + 1}`, () =>
-      console.log(`event listener running on on port ${+PORT + 1}`)
-    );
+  app.use(parser.xml()).all("/wemo", (request, response) => {
+    const sid = request.headers.sid;
+    if (sid) {
+      const binaryState = request.body["e:propertyset"]["e:property"][0];
+      console.log(sid, binaryState);
+      response.sendStatus(200);
+    } else {
+      response.send("hello from the API server.");
+    }
+  });
+
+  const httpServer = http.createServer(app);
+  httpServer.listen(PORT, () => {
+    console.log(`server started on http://localhost:${PORT}`);
+  });
 };
 
 main().catch((error) => console.error(error));
