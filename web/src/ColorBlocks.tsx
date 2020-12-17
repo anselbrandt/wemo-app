61608e4ef96078def9b4f3621497ca1c58868388
anselbrandt <ansel@anselbrandt.com>
2020-12-17 14:20:52 -0500

    Set text color based on HSL lightness

--- web/src/ColorBlocks.tsx
+++ web/src/ColorBlocks.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { colors } from "./colors";
+import { hexToHsl } from "./utils/colorUtils";
 
 export const ColorBlocks: React.FC = () => {
+  const colorPicker = (hex: any) =>
+    hexToHsl(hex)[2].toFixed(0) < 50 ? "white" : "black";
   return (
     <>
       {Object.keys(colors).map((color) => (
@@ -11,12 +14,21 @@ export const ColorBlocks: React.FC = () => {
             {typeof colors[color] !== "string" ? (
               Object.keys(colors[color]).map((hue, index) => (
                 <div
-                  style={{ backgroundColor: colors[color][index] }}
+                  style={{
+                    backgroundColor: colors[color][index],
+                    color: `${colorPicker(colors[color][index])}`,
+                  }}
                 >{`${hue}: ${colors[color][index]}`}</div>
               ))
             ) : (
               <div style={{ backgroundColor: colors[color] as string }}>
-                {colors[color]}
+                <div
+                  style={{
+                    color: `${colorPicker(colors[color])}`,
+                  }}
+                >
+                  {colors[color]}
+                </div>
               </div>
             )}
           </div>
