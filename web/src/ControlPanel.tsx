2aefb313f9433f74c18752412237fcac59633e2a
anselbrandt <ansel@anselbrandt.com>
2020-12-18 18:31:24 -0500

    Add mock device state

--- web/src/ControlPanel.tsx
+++ web/src/ControlPanel.tsx
@@ -10,7 +10,13 @@ export const ControlPanel: React.FC<Props> = ({ devices }) => {
     <Flex width="20rem" justifyContent="center">
       <Grid templateColumns="1fr 1fr" gap={4}>
         {devices.map((device) => (
-          <Button key={device.name} size="lg" h="8rem" colorScheme="nebula">
+          <Button
+            key={device.name}
+            size="lg"
+            h="8rem"
+            colorScheme="nebula"
+            variant={device.state ? "solid" : "outline"}
+          >
             {device.name}
           </Button>
         ))}
--- web/src/Main.tsx
+++ web/src/Main.tsx
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 import { ControlPanel } from "./ControlPanel";
 
 const devices = [
-  { name: "Kitchen", endpoint: "/api/kitchen" },
-  { name: "Piano", endpoint: "/api/piano" },
-  { name: "Dresser", endpoint: "/api/dresser" },
-  { name: "Living", endpoint: "/api/living" },
-  { name: "Corner", endpoint: "/api/corner" },
-  { name: "Nightlight", endpoint: "/api/nightlight" },
+  { name: "Kitchen", endpoint: "/api/kitchen", state: 0 },
+  { name: "Piano", endpoint: "/api/piano", state: 0 },
+  { name: "Dresser", endpoint: "/api/dresser", state: 1 },
+  { name: "Living", endpoint: "/api/living", state: 0 },
+  { name: "Corner", endpoint: "/api/corner", state: 0 },
+  { name: "Nightlight", endpoint: "/api/nightlight", state: 0 },
 ];
 
 interface Props {}
@@ -24,7 +24,8 @@ export const Main: React.FC<Props> = () => {
           direction="column"
           justifyContent="center"
           alignItems="center"
-          mt={10}
+          h="100vh"
+          maxH="60rem"
         >
           <ControlPanel devices={devices} />
           <ChakraLink as={Link} to="/about" color="nebula" mt={10}>
