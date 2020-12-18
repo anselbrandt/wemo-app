4252d5696911c870fea83e09279a5dd3b650917e
anselbrandt <ansel@anselbrandt.com>
2020-12-17 21:23:37 -0500

    Remove CSS

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import styles from "./App.module.css";
 import { ColorBlocks } from "./ColorBlocks";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Box, Grid } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className={styles.app}>
-      <ColorModeSwitcher justifySelf="flex-end" />
-      <div>wemo-app</div>
-      <div>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
         <ColorBlocks />
-      </div>
-    </div>
+      </Grid>
+    </Box>
   );
 }
 
