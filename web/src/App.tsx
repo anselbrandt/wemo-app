c7940140998c3f8edf734ac4801c7151f247d5a0
anselbrandt <ansel@anselbrandt.com>
2020-12-17 20:58:56 -0500

    Add ColorModeSwitcher

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./App.module.css";
 import { ColorBlocks } from "./ColorBlocks";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 function App() {
   return (
     <div className={styles.app}>
+      <ColorModeSwitcher justifySelf="flex-end" />
       <div>wemo-app</div>
       <div>
         <ColorBlocks />
