11c58746e3e032eef9f48a20a4493a95b06dcd4c
anselbrandt <ansel@anselbrandt.com>
2020-12-18 12:39:28 -0500

    Add chakraColors

--- web/src/ColorBlocks.tsx
+++ web/src/ColorBlocks.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { colors } from "./colors";
+// import { colors } from "./chakraColors";
 import { hexToLightness } from "./utils/colorUtils";
 
 export const ColorBlocks: React.FC = () => {
@@ -7,21 +8,30 @@ export const ColorBlocks: React.FC = () => {
     hexToLightness(hex) < 35 ? "white" : "black";
   return (
     <>
-      {Object.keys(colors).map((color) => (
-        <div>
+      {Object.keys(colors).map((color, index) => (
+        <div key={index}>
           <div>{color}</div>
           <div>
             {typeof colors[color] !== "string" ? (
               Object.keys(colors[color]).map((hue, index) => (
                 <div
+                  key={`${hue}${index}`}
                   style={{
-                    backgroundColor: colors[color][index],
-                    color: `${colorPicker(colors[color][index])}`,
+                    backgroundColor:
+                      colors[color][Object.keys(colors[color])[index] as any],
+                    color: `${colorPicker(
+                      colors[color][Object.keys(colors[color])[index] as any]
+                    )}`,
                   }}
-                >{`${hue}: ${colors[color][index]}`}</div>
+                >{`${hue}: ${
+                  colors[color][Object.keys(colors[color])[index] as any]
+                }`}</div>
               ))
             ) : (
-              <div style={{ backgroundColor: colors[color] as string }}>
+              <div
+                key={`${color}`}
+                style={{ backgroundColor: colors[color] as string }}
+              >
                 <div
                   style={{
                     color: `${colorPicker(colors[color])}`,
