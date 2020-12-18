e91395d64a57049f775898a6e20f4d52e1a2f34f
anselbrandt <ansel@anselbrandt.com>
2020-12-17 21:49:55 -0500

    Add 404

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "./Home";
 import { ThemeColors } from "./ThemeColors";
 import { About } from "./About";
+import { Error } from "./Error";
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/colors" exact component={ThemeColors} />
-      <Route path="/about" exact component={About} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/colors" exact component={ThemeColors} />
+        <Route path="/about" exact component={About} />
+        <Route path="/" component={Error} />
+      </Switch>
     </BrowserRouter>
   );
 }
