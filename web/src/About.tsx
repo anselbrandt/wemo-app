10f68dbab9e94ddf0b2152c59be9166ccb78d1bb
anselbrandt <ansel@anselbrandt.com>
2020-12-20 15:24:54 -0500

    Style About

--- web/src/About.tsx
+++ web/src/About.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import { Box, Grid, Link as ChakraLink } from "@chakra-ui/react";
+import { Box, Flex, Grid, Link as ChakraLink } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { Markdown } from "./Markdown";
 
@@ -8,13 +8,15 @@ interface Props {}
 
 export const About: React.FC<Props> = () => {
   return (
-    <Box textAlign="center" fontSize="xl">
+    <Box fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
-        <Markdown />
-        <ChakraLink as={Link} to="/">
-          home
-        </ChakraLink>
+        <Flex direction="column" justifyContent="center" alignItems="center">
+          <Markdown />
+          <ChakraLink as={Link} to="/">
+            home
+          </ChakraLink>
+        </Flex>
       </Grid>
     </Box>
   );
